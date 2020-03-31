c48dcc936a7801217e32b1b4776c3af34f1b5a31
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-31 18:20:36 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,5 +3,3 @@ class Author
 
 
 end
-
-
--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,6 @@
-require_relative "author.rb"
+
 
 class Post
   attr_accessor :title
 
 end
-
-post = Post.new
-post.title = "Hello World"
-post.author = author
